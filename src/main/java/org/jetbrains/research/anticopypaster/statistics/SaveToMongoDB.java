34c6f0aa8d3c0aa3e092812a7cbe0bad889291c4
Benjamin Knobloch <bknobloc@stevens.edu>
2023-07-07 12:38:39 -0400

    Added checks for SSH connecting and client connection

--- src/main/java/org/jetbrains/research/anticopypaster/statistics/SaveToMongoDB.java
+++ src/main/java/org/jetbrains/research/anticopypaster/statistics/SaveToMongoDB.java
@@ -1,9 +1,13 @@
 package org.jetbrains.research.anticopypaster.statistics;
 
 import com.intellij.ide.util.PropertiesComponent;
+
+import java.io.IOException;
+import java.net.Socket;
 import java.util.UUID;
 
 import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoClient;
 import com.mongodb.ConnectionString;
@@ -15,32 +19,39 @@ import org.bson.Document;
 public class SaveToMongoDB {
 
     // TODO: Finish handling connection details.
-    private static final String CONNECTION_STRING = "";
-    private static final String DATABASE_NAME = "";
+    private static final String CONNECTION_STRING = "mongodb://127.0.0.1:27018";
+    private static final String DATABASE_NAME = "anticopypaster";
     private static final String USER_STATISTICS_COLLECTION = "AntiCopyPaster_User_Statistics";
 
     public static void saveStatistics(int notificationCount, int extractMethodAppliedCount, int extractMethodRejectedCount, int copyCount, int pasteCount) {
 
+        if (!isSSHTunnelRunning("127.0.0.1", 27018)) {
+            System.out.println("Cannot save user statistics to database. SSH tunnel has not been established.");
+            return;
+        }
+
         MongoClientSettings settings = MongoClientSettings.builder()
                         .applyConnectionString(new ConnectionString(CONNECTION_STRING))
                                 .build();
-        MongoClient mongoClient = MongoClients.create(settings);
-        MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
-        MongoCollection<Document> statisticsCollection = database.getCollection(USER_STATISTICS_COLLECTION);
 
-        // Update database. Update the document if it exists, or create a new one for the user.
-        String userId = getUserID();
-        Document query = new Document("userId", userId);
-        Document updatedDocument = new Document("userId", userId)
-                .append("notificationCount", notificationCount)
-                .append("extractMethodAppliedCount", extractMethodAppliedCount)
-                .append("extractMethodRejectedCount", extractMethodRejectedCount)
-                .append("copyCount", copyCount)
-                .append("pasteCount", pasteCount);
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
+            MongoCollection<Document> statisticsCollection = database.getCollection(USER_STATISTICS_COLLECTION);
 
-        statisticsCollection.updateOne(query, new Document("$set", updatedDocument), new UpdateOptions().upsert(true));
+            // Update database. Update the document if it exists, or create a new one for the user.
+            String userId = getUserID();
+            Document query = new Document("userId", userId);
+            Document updatedDocument = new Document("userId", userId)
+                    .append("notificationCount", notificationCount)
+                    .append("extractMethodAppliedCount", extractMethodAppliedCount)
+                    .append("extractMethodRejectedCount", extractMethodRejectedCount)
+                    .append("copyCount", copyCount)
+                    .append("pasteCount", pasteCount);
 
-        mongoClient.close();
+            statisticsCollection.updateOne(query, new Document("$set", updatedDocument), new UpdateOptions().upsert(true));
+        }
+        catch (MongoException e) { System.out.println("MongoDB exception occurred."); e.printStackTrace(); }
+        catch (Exception e) { System.out.println("Unexpected exception occurred."); e.printStackTrace(); }
     }
 
     private static String getUserID() {
@@ -52,6 +63,14 @@ public class SaveToMongoDB {
         userId = UUID.randomUUID().toString();
         PropertiesComponent.getInstance().setValue("UniqueUserID", userId);
         return userId;
+    }
+
+    public static boolean isSSHTunnelRunning(String tunnelHost, int tunnelPort) {
+        try (Socket socket = new Socket(tunnelHost, tunnelPort)) {
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
     }
 
 }
