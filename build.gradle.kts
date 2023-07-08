8a37035c0b90cd30efc4efba9bd72be9e9b71929
Thomas Kain <tkain@stevens.edu>
2023-07-08 00:04:14 -0400

    Updated dependencies and Gradle plugins
    * IntelliJ plugin to 1.15.0
    * Gradle Test Logger to 3.2.0
    * GSON to 2.10.1
    * Apache Commons to 3.12.0
    * PMML4S to 1.0.1 (/w Scala 3)
    * JUnit to 5.9.3
    * Mockito to 5.4.0 (mockito-core supersedes mockito-inline)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.jetbrains.intellij") version "1.13.2"
+    id("org.jetbrains.intellij") version "1.15.0"
     id("com.adarshr.test-logger") version "3.2.0"
 }
 
@@ -18,9 +18,9 @@ repositories {
 }
 
 dependencies {
-    implementation("com.google.code.gson:gson:2.8.6")
-    implementation("org.apache.commons:commons-lang3:3.0")
-    implementation("org.pmml4s:pmml4s_2.13:0.9.10")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.apache.commons:commons-lang3:3.12.0")
+    implementation("org.pmml4s:pmml4s_3:1.0.1")
     /**
      * This file is commented out as it uses the TensorFlow API. By removing that dependency,
      * the plugin will be a fifth of the size and much more lightweight, but this won't
@@ -30,12 +30,12 @@ dependencies {
     // implementation("org.tensorflow:tensorflow:1.15.0")
 
     // Test dependencies
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
-    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.9.2")
-    testImplementation("org.mockito:mockito-inline:4.0.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:4.0.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.9.3")
+    testImplementation("org.mockito:mockito-core:5.4.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.4.0")
 }
 
 fun properties(key: String) = project.findProperty(key).toString()
