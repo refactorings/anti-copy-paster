eb94d329171088e1887dc5f6b38e1c137f72bf7d
Thomas Kain <tkain@stevens.edu>
2023-07-11 02:14:21 -0400

    Add project shutdown hook for statistics upload
    * Leverages IntelliJ's disposability system to trigger upon project shutdown
    * Statistics are only sent if they haven't been sent in over 3 days

--- src/main/java/org/jetbrains/research/anticopypaster/statistics/AntiCopyPasterUsageStatistics.java
+++ src/main/java/org/jetbrains/research/anticopypaster/statistics/AntiCopyPasterUsageStatistics.java
@@ -1,5 +1,6 @@
 package org.jetbrains.research.anticopypaster.statistics;
 
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.Service;
 import com.intellij.openapi.components.State;
@@ -8,12 +9,18 @@ import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Stores information about the plugin usage on the project level in the ./idea/anticopypaster-plugin.xml file.
  */
 @Service(Service.Level.PROJECT)
 @State(name = "AntiCopyPasterUsageStatistics", storages = {@Storage("anticopypaster-plugin-usage.xml")})
-public class AntiCopyPasterUsageStatistics implements PersistentStateComponent<AntiCopyPasterUsageStatistics.PluginState> {
+public final class AntiCopyPasterUsageStatistics implements PersistentStateComponent<AntiCopyPasterUsageStatistics.PluginState>, Disposable {
+
+    /** Specifies the minimum time between usage statistics transmissions to the server. */
+    private static final long TRANSMISSION_INTERVAL = TimeUnit.MILLISECONDS.convert(3, TimeUnit.DAYS);
+
     private PluginState usageState = new PluginState();
 
     @Override
@@ -50,12 +57,22 @@ public class AntiCopyPasterUsageStatistics implements PersistentStateComponent<A
         usageState.onPaste();
     }
 
+    @Override
+    public void dispose() {
+        long now = System.currentTimeMillis();
+        if (now - usageState.lastTransmissionTime >= TRANSMISSION_INTERVAL) {
+            usageState.saveToMongoDB();
+            usageState.lastTransmissionTime = now;
+        }
+    }
+
     public static class PluginState {
         public int notificationCount = 0;
         public int extractMethodAppliedCount = 0;
         public int extractMethodRejectedCount = 0;
         public int copyCount = 0;
         public int pasteCount = 0;
+        public long lastTransmissionTime = 0;
 
         public void notification() {
             notificationCount += 1;
