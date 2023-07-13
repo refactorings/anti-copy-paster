75b5eaec81fe8476cd25d011bf384a1f890fd1b3
Jonathan Memoli <jonomemoli3@gmail.com>
2023-07-12 21:41:45 -0400

    Implemented JSch to handle SSH connection with mongodb server
    Connection string was slightly modified

--- src/main/java/org/jetbrains/research/anticopypaster/statistics/SaveToMongoDB.java
+++ src/main/java/org/jetbrains/research/anticopypaster/statistics/SaveToMongoDB.java
@@ -2,12 +2,16 @@ package org.jetbrains.research.anticopypaster.statistics;
 
 import com.intellij.ide.util.PropertiesComponent;
 
+import com.jcraft.jsch.*;
+
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.UUID;
 
 import com.intellij.openapi.project.Project;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoException;
 import com.mongodb.client.MongoClients;
@@ -23,41 +27,69 @@ public class SaveToMongoDB {
 
     private static  String username;
     private static  String password;
-    private static final String hostPort = "155.246.39.61:27018";
+    private static final String sshHost = "155.246.39.61:27018";
+    private static final int sshPort = 22;
+    private static final String mongodbHost = "localhost";
+    private static final int mongodbPort = 27017;
+    private static final int forwardPort = 27017;
     private static final String DATABASE_NAME = "anticopypaster";
     private static final String USER_STATISTICS_COLLECTION = "AntiCopyPaster_User_Statistics";
 
     public static void saveStatistics(Project project, int notificationCount, int extractMethodAppliedCount, int extractMethodRejectedCount, int copyCount, int pasteCount) {
-
+        //Get password and username from ProjectSettingsState
+        //TODO: possibly add extra security to how username and passwords are stored
         getUsernameAndPassword(project);
+        try {
+            //First establish an SSH connection with the stevens server
+            JSch jsch = new JSch();
+            Session session = jsch.getSession(username, sshHost, sshPort);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
 
-        MongoClientSettings settings = MongoClientSettings.builder()
-                        .applyConnectionString(new ConnectionString(Objects.requireNonNull(makeConnectionString())))
-                                .build();
+            session.connect();
+            System.out.println("Connected");
 
-        try (MongoClient mongoClient = MongoClients.create(settings)) {
-            MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
-            MongoCollection<Document> statisticsCollection = database.getCollection(USER_STATISTICS_COLLECTION);
+            //Forward remote 27017 port to local 27017
+            session.setPortForwardingL(forwardPort, mongodbHost, mongodbPort);
+            System.out.println("Port forwarding successful");
 
-            // Update database. Update the document if it exists, or create a new one for the user.
-            String userId = getUserID();
-            Document query = new Document("userId", userId);
-            Document updatedDocument = new Document("userId", userId)
-                    .append("notificationCount", notificationCount)
-                    .append("extractMethodAppliedCount", extractMethodAppliedCount)
-                    .append("extractMethodRejectedCount", extractMethodRejectedCount)
-                    .append("copyCount", copyCount)
-                    .append("pasteCount", pasteCount);
+            MongoClientSettings settings = MongoClientSettings.builder()
+                    .applyConnectionString(new ConnectionString(Objects.requireNonNull(makeConnectionString())))
+                    .build();
 
-            statisticsCollection.updateOne(query, new Document("$set", updatedDocument), new UpdateOptions().upsert(true));
+            try (MongoClient mongoClient = MongoClients.create(settings)) {
+                MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
+                MongoCollection<Document> statisticsCollection = database.getCollection(USER_STATISTICS_COLLECTION);
+
+                // Update database. Update the document if it exists, or create a new one for the user.
+                String userId = getUserID();
+                Document query = new Document("userId", userId);
+                Document updatedDocument = new Document("userId", userId)
+                        .append("notificationCount", notificationCount)
+                        .append("extractMethodAppliedCount", extractMethodAppliedCount)
+                        .append("extractMethodRejectedCount", extractMethodRejectedCount)
+                        .append("copyCount", copyCount)
+                        .append("pasteCount", pasteCount);
+
+                statisticsCollection.updateOne(query, new Document("$set", updatedDocument), new UpdateOptions().upsert(true));
+            } catch (MongoException e) {
+                System.err.println("MongoDB exception occurred.");
+                e.printStackTrace();
+            } catch (Exception e) {
+                System.err.println("Unexpected exception occurred.");
+                e.printStackTrace();
+            }
+            session.disconnect();
+            System.out.println("Session done");
+        }catch (JSchException e) {
+            System.err.println("Failed to establish SSH connection: " + e.getMessage());
+            e.printStackTrace();
         }
-        catch (MongoException e) { System.err.println("MongoDB exception occurred."); e.printStackTrace(); }
-        catch (Exception e) { System.err.println("Unexpected exception occurred."); e.printStackTrace(); }
     }
     private static String makeConnectionString() {
         try {
             return "mongodb://" + URLEncoder.encode(username, StandardCharsets.UTF_8) + ":" +
-                    URLEncoder.encode(password, StandardCharsets.UTF_8) + "@" + hostPort + "/?authSource=admin";
+                    URLEncoder.encode(password, StandardCharsets.UTF_8) + "@localhost/?authSource=admin";
         } catch (Exception e) {
             System.err.println("Error encoding username or password.");
             e.printStackTrace();
