6096122875ab921bc4bfb2c744b6045260f13083
Thomas Kain <tkain@stevens.edu>
2023-07-21 22:11:45 -0400

    Removed SSH tunneling functionality
    Thanks to IT, we no longer need it to connect to our database.
    Also, removed an unnecessary requireNonNull.

--- src/main/java/org/jetbrains/research/anticopypaster/statistics/AntiCopyPasterTelemetry.java
+++ src/main/java/org/jetbrains/research/anticopypaster/statistics/AntiCopyPasterTelemetry.java
@@ -6,16 +6,12 @@ import com.intellij.ide.passwordSafe.PasswordSafe;
 import com.intellij.ide.util.PropertiesComponent;
 
 import com.intellij.openapi.startup.StartupActivity;
-import com.jcraft.jsch.*;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.Objects;
 import java.util.UUID;
 
 import com.intellij.openapi.project.Project;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.Session;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoException;
 import com.mongodb.client.MongoClients;
@@ -33,13 +29,9 @@ import static org.jetbrains.research.anticopypaster.statistics.AntiCopyPasterUsa
 
 public class AntiCopyPasterTelemetry implements StartupActivity.DumbAware {
 
-    private static  String username;
-    private static  String password;
-    private static final String sshHost = "155.246.39.61";
-    private static final int sshPort = 22;
-    private static final String mongodbHost = "localhost";
-    private static final int mongodbPort = 27017;
-    private static final int forwardPort = 27017;
+    private static String username;
+    private static String password;
+    private static final String REMOTE_HOST = "155.246.39.61";
     private static final String DATABASE_NAME = "anticopypaster";
     private static final String USER_STATISTICS_COLLECTION = "AntiCopyPaster_User_Statistics";
 
@@ -62,63 +54,38 @@ public class AntiCopyPasterTelemetry implements StartupActivity.DumbAware {
     public static void saveStatistics(Project project, int notificationCount, int extractMethodAppliedCount, int extractMethodRejectedCount, int copyCount, int pasteCount) {
         //Get password and username from PasswordSafe
         getUsernameAndPassword(project);
-        try {
-            //First establish an SSH connection with the stevens server
-            JSch jsch = new JSch();
-            Session session = jsch.getSession(username, sshHost, sshPort);
-            session.setPassword(password);
-            session.setConfig("StrictHostKeyChecking", "no");
 
-            session.connect();
-            System.out.println("Connected");
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(makeConnectionString()))
+                .build();
 
-            //Forward remote 27017 port to local 27017
-            session.setPortForwardingL(forwardPort, mongodbHost, mongodbPort);
-            System.out.println("Port forwarding successful");
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
+            MongoCollection<Document> statisticsCollection = database.getCollection(USER_STATISTICS_COLLECTION);
 
-            MongoClientSettings settings = MongoClientSettings.builder()
-                    .applyConnectionString(new ConnectionString(Objects.requireNonNull(makeConnectionString())))
-                    .build();
+            // Update database. Update the document if it exists, or create a new one for the user.
+            String userId = getUserID();
+            Document query = new Document("userId", userId);
+            Document updatedDocument = new Document("userId", userId)
+                    .append("notificationCount", notificationCount)
+                    .append("extractMethodAppliedCount", extractMethodAppliedCount)
+                    .append("extractMethodRejectedCount", extractMethodRejectedCount)
+                    .append("copyCount", copyCount)
+                    .append("pasteCount", pasteCount);
 
-            try (MongoClient mongoClient = MongoClients.create(settings)) {
-                MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
-                MongoCollection<Document> statisticsCollection = database.getCollection(USER_STATISTICS_COLLECTION);
-
-                // Update database. Update the document if it exists, or create a new one for the user.
-                String userId = getUserID();
-                Document query = new Document("userId", userId);
-                Document updatedDocument = new Document("userId", userId)
-                        .append("notificationCount", notificationCount)
-                        .append("extractMethodAppliedCount", extractMethodAppliedCount)
-                        .append("extractMethodRejectedCount", extractMethodRejectedCount)
-                        .append("copyCount", copyCount)
-                        .append("pasteCount", pasteCount);
-
-                statisticsCollection.updateOne(query, new Document("$set", updatedDocument), new UpdateOptions().upsert(true));
-            } catch (MongoException e) {
-                System.err.println("MongoDB exception occurred.");
-                e.printStackTrace();
-            } catch (Exception e) {
-                System.err.println("Unexpected exception occurred.");
-                e.printStackTrace();
-            }
-            session.disconnect();
-            System.out.println("Session done");
-        }catch (JSchException e) {
-            System.err.println("Failed to establish SSH connection: " + e.getMessage());
+            statisticsCollection.updateOne(query, new Document("$set", updatedDocument), new UpdateOptions().upsert(true));
+        } catch (MongoException e) {
+            System.err.println("MongoDB exception occurred.");
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Unexpected exception occurred.");
             e.printStackTrace();
         }
     }
+
     private static String makeConnectionString() {
-        try {
-            return "mongodb://" + URLEncoder.encode(username, StandardCharsets.UTF_8) + ":" +
-                    URLEncoder.encode(password, StandardCharsets.UTF_8) + "@localhost/?authSource=admin";
-        } catch (Exception e) {
-            System.err.println("Error encoding username or password.");
-            e.printStackTrace();
-            // Handle the exception appropriately, such as throwing a custom exception or returning a default value
-            return null;
-        }
+        return "mongodb://" + URLEncoder.encode(username, StandardCharsets.UTF_8) + ":" +
+                URLEncoder.encode(password, StandardCharsets.UTF_8) + "@" + REMOTE_HOST + "/?authSource=admin";
     }
 
     private static void getUsernameAndPassword(Project project) {
@@ -141,5 +108,4 @@ public class AntiCopyPasterTelemetry implements StartupActivity.DumbAware {
         PropertiesComponent.getInstance().setValue("UniqueUserID", userId);
         return userId;
     }
-
 }
