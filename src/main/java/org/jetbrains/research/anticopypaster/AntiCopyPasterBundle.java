8cda172fe5cd30f15e4ae80c17b20519f1ff2070
Benjamin Knobloch <bknobloc@stevens.edu>
2023-07-26 14:59:25 -0400

    Finished implementation of .properties text

--- src/main/java/org/jetbrains/research/anticopypaster/AntiCopyPasterBundle.java
+++ src/main/java/org/jetbrains/research/anticopypaster/AntiCopyPasterBundle.java
@@ -22,9 +22,6 @@ public final class AntiCopyPasterBundle {
     private static ResourceBundle getBundle() {
         ResourceBundle bundle = SoftReference.dereference(INSTANCE);
         if (bundle == null) {
-            //Locale locale = new Locale("es");
-            //System.out.println("Searching for resource bundle with locale: " + locale);
-            //bundle = ResourceBundle.getBundle(BUNDLE, new Locale("es"));
             bundle = ResourceBundle.getBundle(BUNDLE);
             INSTANCE = new SoftReference<>(bundle);
         }
