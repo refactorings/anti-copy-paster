397229aad9ac2f385d5308f8b8aef78cca2ef116
Jacob Ashkenas <jacobashkenas@gmail.com>
2023-10-29 21:06:50 -0400

    Clone type one detection via tree traversal

--- src/main/java/org/jetbrains/research/anticopypaster/ide/DuplicatesInspection.java
+++ src/main/java/org/jetbrains/research/anticopypaster/ide/DuplicatesInspection.java
@@ -6,13 +6,12 @@ import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.IncorrectOperationException;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 public final class DuplicatesInspection {
     private static final Logger LOG = Logger.getInstance(AntiCopyPastePreProcessor.class);
-
-//    private final ThreadPoolExecutor pool = (ThreadPoolExecutor) Executors.newFixedThreadPool(8);
 
     /**
      * Searches for duplicates in methods extracted from the file.
@@ -25,65 +24,70 @@ public final class DuplicatesInspection {
      * @return the result of duplicates' detection.
      */
     public InspectionResult resolve(PsiFile file, final String code) {
-//        final List<String> tokensOfPastedCode = getTokens(code);
-//        @NotNull Collection<PsiMethod> methods = PsiTreeUtil.findChildrenOfType(file, PsiMethod.class);
-//        final List<DuplicateResult> results = methods.stream()
-//                .map(method -> new DuplicateResultComputable(code, method, tokensOfPastedCode))
-//                .map(computable -> pool.submit(() -> ApplicationManager.getApplication().runReadAction(computable)))
-//                .map(future -> {
-//                    try {
-//                        return future.get();
-//                    } catch (Exception e) {
-//                        LOG.warn("[ACP] Failed while searching for code duplicates.", e);
-//                        return null;
-//                    }
-//                })
-//                .filter(Objects::nonNull)
-//                .collect(Collectors.toList());
-//        return new InspectionResult(results);
         ArrayList<DuplicateResult> results = new ArrayList<>();
         try {
             PsiCodeBlock block = PsiElementFactory.getInstance(file.getProject())
                     .createCodeBlockFromText("{" + code + "}", file.getContext());
-            PsiElement blockStart = block.getFirstChild();
-            while (blockStart instanceof PsiWhiteSpace || blockStart instanceof PsiComment)
-                blockStart = blockStart.getNextSibling();
-            if (blockStart == null) return new InspectionResult(results);
+            if (block.isEmpty()) return new InspectionResult(results);
+            PsiElement blockStart = block.getStatements()[0];
             Collection<PsiElement> matches = PsiTreeUtil.findChildrenOfType(file, blockStart.getClass());
             for (PsiElement match : matches) {
-                PsiElement end = isDuplicateAt(block, match);
-                if (end != null)
-                    results.add(new DuplicateResult(match, end));
+                PsiElement end = isDuplicateAt(block, blockStart, match);
+                if (end != null) results.add(new DuplicateResult(match, end));
             }
         } catch (IncorrectOperationException ex) {
             LOG.error(ex);
             return new InspectionResult(results);
         }
 
-        LOG.info("[ACP] Found " + results.size() + " duplicates");
-
         return new InspectionResult(results);
     }
 
-    public PsiElement nextNonEmptyNorCommentLeaf(PsiElement current) {
-        do {
-            current = PsiTreeUtil.nextVisibleLeaf(current);
-        } while (current instanceof PsiComment);
-        return current;
+    /**
+     * Gets all the children of an element that aren't whitespace or comments.
+     * This conforms to the specifications of type 1 clones.
+     *
+     * @return The viable children for tree matching.
+     */
+    private List<PsiElement> viableChildren(PsiElement element) {
+        if (element == null) return new ArrayList<>();
+        return Arrays.stream(element.getChildren()).filter(psiElement ->
+                !(psiElement instanceof PsiWhiteSpace || psiElement instanceof PsiComment)).toList();
     }
 
-    public PsiElement isDuplicateAt(PsiCodeBlock fragment, PsiElement start) {
-        PsiElement fragCurrent = fragment.getFirstBodyElement();
-        PsiElement dupeCurrent = start;
-        if (fragCurrent != null && fragCurrent.getClass() != dupeCurrent.getClass())
-            return null;
-        while (fragCurrent != fragment.getLastBodyElement()) {
-            fragCurrent = nextNonEmptyNorCommentLeaf(fragCurrent);
-            dupeCurrent = nextNonEmptyNorCommentLeaf(dupeCurrent);
-            if (dupeCurrent == null) return null;
-            if (!fragCurrent.textMatches(dupeCurrent)) return null;
+    /**
+     * Determines if two elements are an exact text match through tree traversal.
+     * @return Whether the elements match
+     */
+    private boolean exactMatch(PsiElement a, PsiElement b) {
+        if (a == null || b == null) return false;
+        // Filter children of each element.
+        List<PsiElement> childrenA = viableChildren(a);
+        List<PsiElement> childrenB = viableChildren(b);
+        // No need to traverse if different number of children.
+        if (childrenA.size() != childrenB.size()) return false;
+        if (childrenA.size() == 0) return a.textMatches(b);
+        for (int i = 0; i < childrenA.size(); i++) {
+            if (!exactMatch(childrenA.get(i), childrenB.get(i))) return false;
         }
-        return dupeCurrent;
+        return true;
+    }
+
+    /**
+     * Check for a clone of the fragment starting at the given element.
+     * @return The last member element of the clone
+     */
+    private PsiElement isDuplicateAt(PsiCodeBlock block, PsiElement fragment, PsiElement start) {
+        if (fragment == null) return null;
+        PsiElement fragCurrent = fragment;
+        PsiElement dupeCurrent = start;
+        while (fragCurrent != null && fragCurrent != block.getRBrace()) {
+            if (dupeCurrent == null) return null;
+            if (!exactMatch(fragCurrent, dupeCurrent)) return null;
+            fragCurrent = fragCurrent.getNextSibling();
+            dupeCurrent = dupeCurrent.getNextSibling();
+        }
+        return dupeCurrent.getPrevSibling();
     }
 
     public record DuplicateResult(PsiElement start, PsiElement end) {}
@@ -99,47 +103,4 @@ public final class DuplicatesInspection {
             return this.results.size();
         }
     }
-
-//    private static class DuplicateResultComputable implements Computable<DuplicateResult> {
-//        private final String code;
-//        private final PsiMethod psiMethod;
-//        private final List<String> tokensOfPastedCode;
-//
-//        private DuplicateResultComputable(String code, PsiMethod psiMethod, List<String> tokensOfPastedCode) {
-//            this.code = code;
-//            this.psiMethod = psiMethod;
-//            this.tokensOfPastedCode = tokensOfPastedCode;
-//        }
-
-//        @Override
-//        public DuplicateResult compute() {
-//            DuplicateResult duplicateResult = null;
-//            PsiCodeBlock methodBody = psiMethod.getBody();
-//            if (methodBody != null) {
-//                String rawCode =
-//                        code.replace('\n', ' ').replace('\t', ' ')
-//                                .replace('\r', ' ').replaceAll("\\s+", "");
-//                String rawMethodBody = psiMethod.getText().replace('\n', ' ').replace('\t', ' ')
-//                        .replace('\r', ' ').replaceAll("\\s+", "");
-//                boolean matches = StringUtils.contains(rawMethodBody, rawCode);
-//                if (matches) {
-//                    duplicateResult = new DuplicateResult(psiMethod, 1.0);
-//                } else {
-//                    List<String> tokensOfMethod = getTokens(methodBody.getText());
-//                    double maxNumOfTokens = Math.max(tokensOfPastedCode.size(), tokensOfMethod.size());
-//                    // Calculates the intersection of tokens
-//                    tokensOfMethod.retainAll(tokensOfPastedCode);
-//                    double threshold = tokensOfMethod.size() / maxNumOfTokens;
-//                    if (threshold >= 0.8) {
-//                        duplicateResult = new DuplicateResult(psiMethod, threshold);
-//                    }
-//                }
-//            }
-//            return duplicateResult;
-//        }
-//    }
-
-//    private static List<String> getTokens(String text) {
-//        return StringUtil.getWordsIn(text);
-//    }
 }
