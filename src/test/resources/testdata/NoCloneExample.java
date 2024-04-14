eaf8a3b73b894e3a7378192e2f351ba1081bf9c5
markfalletta <markcfalletta@gmail.com>
2024-04-14 15:46:13 -0400

    Standardized NoClone test file
    Signed-off-by: markfalletta <markcfalletta@gmail.com>

---
+++ src/test/resources/testdata/NoCloneExample.java
@@ -0,0 +1,20 @@
+import com.intellij.testFramework.TestDataFile;
+
+@TestDataFile
+public class test {
+
+    public float prod(float s, float p) {
+        float result = s * p;
+        return result;
+    }
+
+    public int sub(int toSub){
+        this.currentNumber -= toAdd;
+        return this.currentNumber;
+    }
+
+    public int add(int toAdd){
+        this.currentNumber += toAdd;
+        return this.currentNumber;
+    }
+}
