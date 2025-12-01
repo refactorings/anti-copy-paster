2969b831dc49a8dab385eeac7e9a8b7aa8b71ea2
xqian7 <xqian7@stevens.edu>
2025-12-01 12:56:41 -0500

    Update ProjectSettingsComponent.java

--- src/main/java/org/jetbrains/research/anticopypaster/config/ProjectSettingsComponent.java
+++ src/main/java/org/jetbrains/research/anticopypaster/config/ProjectSettingsComponent.java
@@ -561,12 +561,12 @@ public class ProjectSettingsComponent {
                             "gpt-4o", "gpt-4o-mini", "gpt-5", "gpt-5-chat", "gpt-5-nano", "gpt-5-mini", "o1", "o1-mini", "o3", "o3-mini", "o4-mini"
                     }));
                     case "Gemini" -> aidermodelComboBox.setModel(new DefaultComboBoxModel<>(new String[] {
-                            "gemini-1.5-pro", "gemini-1.5-flash", "gemini-2.5-pro", "gemini-2.5-pro-exp-03-25", "gemini-2.5-flash", "gemini-2.5-flash-lite-preview-06-17", "gemini-2.5-flash-lite-preview-09-2025", "gemini-2.5-flash-preview-09-2025"
+                            "gemini-1.5-pro", "gemini-1.5-flash", "gemini-2.5-pro", "gemini-2.5-pro-exp-03-25", "gemini-2.5-flash", "gemini-2.5-flash-lite-preview-06-17", "gemini-2.5-flash-lite-preview-09-2025", "gemini-2.5-flash-preview-09-2025", "gemini-3-pro-preview"
                     }));
                     case "Anthropic" -> aidermodelComboBox.setModel(new DefaultComboBoxModel<>(new String[] {
                            "claude-3-5-haiku-latest", "claude-3-5-sonnet-latest", "claude-3-7-sonnet-20250219",
                             "claude-3-7-sonnet-latest", "claude-3-opus-latest", "claude-3-sonnet-20240229", "claude-4-opus-20250514", "claude-4-sonnet-20250514",
-                            "claude-opus-4-1", "claude-opus-4-1-20250805", "claude-haiku-4-5", "claude-sonnet-4-5", "claude-sonnet-4-5-20250929",
+                            "claude-opus-4-1", "claude-opus-4-1-20250805", "claude-opus-4-5-20251101", "claude-haiku-4-5", "claude-sonnet-4-5", "claude-sonnet-4-5-20250929",
                     }));
                     case "DeepSeek" -> aidermodelComboBox.setModel(new DefaultComboBoxModel<>(new String[] {
                             "deepseek-chat", "deepseek-coder", "deepseek-reasoner", "deepseek-v3"
